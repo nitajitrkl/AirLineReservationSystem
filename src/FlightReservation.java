882bc9d4bd40e6467f1c6fa942c88ae3bd01d63b
Ajit Behera <nitajitrkl@gmail.com>
2024-04-28 10:28:52 -0400

    28TH APRIL

--- src/FlightReservation.java
+++ src/FlightReservation.java
@@ -10,24 +10,6 @@ import java.util.List;
 import java.util.Scanner;
 
 public class FlightReservation implements DisplayClass {
-
-    //        ************************************************************ Fields ************************************************************
-    Flight flight = new Flight();
-    int flightIndexInFlightList;
-
-    //        ************************************************************ Behaviours/Methods ************************************************************
-
-
-    /**
-     * Book the numOfTickets for said flight for the specified user. Update the available seats in main system by
-     * Subtracting the numOfTickets from the main system. If a new customer registers for the flight, then it adds
-     * the customer to that flight, else if the user is already added to that flight, then it just updates the
-     * numOfSeats of that flight.
-     *
-     * @param flightNo     FlightID of the flight to be booked
-     * @param numOfTickets number of tickets to be booked
-     * @param userID       userID of the user which is booking the flight
-     */
     void bookFlight(String flightNo, int numOfTickets, String userID) {
         boolean isFound = false;
         for (Flight f1 : flight.getFlightList()) {
@@ -59,13 +41,6 @@ public class FlightReservation implements DisplayClass {
             System.out.printf("\n %50s You've booked %d tickets for Flight \"%5s\"...", "", numOfTickets, flightNo.toUpperCase());
         }
     }
-
-    /**
-     * Cancels the flight for a particular user and return/add the numOfTickets back to
-     * the main flight scheduler.
-     *
-     * @param userID    ID of the user for whom the flight is to be cancelled
-     */
     void cancelFlight(String userID) {
         String flightNum = "";
         Scanner read = new Scanner(System.in);
@@ -151,7 +126,6 @@ public class FlightReservation implements DisplayClass {
         }
     }
 
-    /*toString() Method for displaying number of flights registered by single user...*/
     public String toString(int serialNum, Flight flights, Customer customer) {
         return String.format("| %-5d| %-41s | %-9s | \t%-9d | %-21s | %-22s | %-10s  |   %-6sHrs |  %-4s  | %-10s |", serialNum, flights.getFlightSchedule(), flights.getFlightNumber(), customer.numOfTicketsBookedByUser.get(serialNum - 1), flights.getFromWhichCity(), flights.getToWhichCity(), flights.fetchArrivalTime(), flights.getFlightTime(), flights.getGate(), flightStatus(flights));
     }
@@ -174,7 +148,6 @@ public class FlightReservation implements DisplayClass {
         }
     }
 
-    /*overloaded toString() method for displaying all users in a flight....*/
 
     public String toString(int serialNum, Customer customer, int index) {
         return String.format("%10s| %-10d | %-10s | %-32s | %-7s | %-27s | %-35s | %-23s |       %-7s  |", "", (serialNum + 1), customer.randomIDDisplay(customer.getUserID()), customer.getName(),
@@ -226,17 +199,6 @@ public class FlightReservation implements DisplayClass {
         }
         return i;
     }
-
-//    @Override
-//    public void displayRegisteredUsersForASpecificFlight(String flightNum) {
-//        System.out.println();
-//        for (Flight flight : flight.getFlightList()) {
-//            List<Customer> c = flight.getListOfRegisteredCustomersInAFlight();
-//            if (flight.getFlightNumber().equalsIgnoreCase(flightNum)) {
-//                displayHeaderForUsers(flight, c);
-//            }
-//        }
-//    }
 
     @Override
     public void displayArtWork(int option) {
@@ -334,7 +296,8 @@ public class FlightReservation implements DisplayClass {
                                                                                                     \s
                     """;
         }
-
         System.out.println(artWork);
     }
+    Flight flight = new Flight();
+    int flightIndexInFlightList;
 }
